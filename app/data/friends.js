6a7a9542bd0b8db228aea1fd9c1578e168670410
tim-garner <timothy.garner602@gmail.com>
2019-06-10 00:11:50 -0400

    updated all friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,6 +12,36 @@ var friendsInfo = [
 name: "John Daly",
     photo: "https://www.google.com/search?q=john+daly&source=lnms&tbm=isch&sa=X&ved=0ahUKEwitqZeB5d3iAhWQmeAKHfUUB04Q_AUIESgC&biw=1540&bih=718#imgrc=ZGjeRrUa1wvZPM:",
     scores: [5,3,4,5,5,1,5,5,1,5,]
+},
+{
+name: "Captain Planet",
+    photo: "https://www.google.com/search?q=captain+planet&source=lnms&tbm=isch&sa=X&ved=0ahUKEwii4Zevgt7iAhUIEawKHYnLAlQQ_AUIESgC&biw=1263&bih=718#imgrc=QZxN8YuyqlcsiM:",
+    scores: [1,1,1,1,1,1,1,1,1,1,]
+
+},
+{
+    name: "Bob The Builder",
+    photo: "https://www.google.com/search?q=bob+the+builder&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjuv9iYg97iAhVBmeAKHSH0BIYQ_AUIECgB&biw=1263&bih=718#imgrc=mrKBQxMKoO6pFM:",
+    scores: [3,1,5,4,2,3,1,2,4,5,]
+},
+
+{
+    name: "Alex Trebek",
+    photo: "https://www.google.com/search?q=alex+trebek&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj0_Mn9gt7iAhVtleAKHYk9B78Q_AUIESgC&biw=1263&bih=718&dpr=1.25#imgrc=aq4VhwCaVesrWM:",
+    scores: [3,3,3,3,3,3,3,3,3,3,]
+},
+{
+    name: "Drew Carey",
+    photo: "https://www.google.com/search?q=drew+carey&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjA3_Pug97iAhXvguAKHWHVDTQQ_AUIESgC&biw=1263&bih=718#imgrc=-2ltDhEcFX2TBM:",
+    scores: [2,2,2,2,2,2,2,2,2,2,]
+},
+
+{
+    name: "Yokozuna",
+    photo: "https://www.google.com/search?q=yokozuna&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi-hPXThd7iAhWjc98KHWvoAhsQ_AUIECgB&biw=1263&bih=718#imgrc=e-sMA4J5rwRblM:",
+    scores: [4,4,4,4,4,4,4,4,4,4,]
 }
+
+
 
 ];
