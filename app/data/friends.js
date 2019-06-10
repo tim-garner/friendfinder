a4cea5a1c9e4f66044a7c8cae0a365319804abc0
tim-garner <timothy.garner602@gmail.com>
2019-06-09 21:55:27 -0400

    updated characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,5 +6,12 @@ var friendsInfo = [
     photo: "https://www.theatlantic.com/politics/archive/2013/01/the-operatic-life-of-richard-nixon/266963/",
     scores: [5,5,5,5,5,5,5,5,5,5,]
 
+},
+
+{
+name: "John Daly",
+    photo: "https://www.google.com/search?q=john+daly&source=lnms&tbm=isch&sa=X&ved=0ahUKEwitqZeB5d3iAhWQmeAKHfUUB04Q_AUIESgC&biw=1540&bih=718#imgrc=ZGjeRrUa1wvZPM:",
+    scores: [5,3,4,5,5,1,5,5,1,5,]
 }
+
 ];
