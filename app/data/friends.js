81780adbb95f331ad462e19c5c201e2b818f3824
tim-garner <timothy.garner602@gmail.com>
2019-06-09 21:41:22 -0400

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,10 @@
 console.log("working");
+
+var friendsInfo = [
+    {
+    name: "Richard Nixon",
+    photo: "https://www.theatlantic.com/politics/archive/2013/01/the-operatic-life-of-richard-nixon/266963/",
+    scores: [5,5,5,5,5,5,5,5,5,5,]
+
+}
+];
