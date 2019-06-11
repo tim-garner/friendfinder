9de2771e11b13ff66c5c6cd5f3a89326580beea2
tim-garner <timothy.garner602@gmail.com>
2019-06-10 22:03:15 -0400

    this is a update

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,1 +1,13 @@
 console.log("working");
+
+var path= require("path");
+
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
+
+	app.get("../api/friends", function(req, res) {
+		res.json(friends);
+    });
+}
+
